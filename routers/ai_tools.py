5fae7c4499021f7dd9593f6b33f347f5c39290be
blorangetech <blorangetech@gmail.com>
2025-07-31 14:02:53 +0900

    fix aitools -3

--- routers/ai_tools.py
+++ routers/ai_tools.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-import httpx
+import httpx, logging
 
 router = APIRouter(prefix="/aitools", tags=["aitools"])
+
+logger = logging.getLogger(__name__)
 
 class RequestModel(BaseModel):
     url: str
@@ -11,5 +13,6 @@ class RequestModel(BaseModel):
 async def search_internet(request: RequestModel):
     async with httpx.AsyncClient() as client:
         response = await client.get(request.url)
+        logger.info(f"GPT use fetchHtml Tool")
         result = response.text
         return {"html": result}
